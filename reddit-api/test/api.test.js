81746785cbb627a6eacff3f9ce15ecdf2387f351
mohamed96321 <atefm.me499@gmail.com>
2024-01-01 15:26:19 +0200

    Fix test issue (app.address is not function) and Now test work fine

--- reddit-api/test/api.test.js
+++ reddit-api/test/api.test.js
@@ -1,42 +1,47 @@
 const chai = require("chai");
-// cons expect = chai.expect;
-const should = chai.should();
+const expect = chai.expect;
 const chaiHttp = require("chai-http");
-const server = require("../server");
+const app = require("../api/app");
 
 chai.use(chaiHttp);
 
-describe("Reddit API", function () {
-  it("should fetch and store Reddit posts", function (done) {
-    chai
-      .request(server)
-      .get("/api/fetch-reddit-posts")
-      .end(function (err, res) {
-        res.should.have.status(200);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success", true);
-        res.body.should.have.property(
-          "message",
-          "Reddit posts fetched and stored successfully.",
-        );
-        res.body.should.have.property("redditPosts");
-        done();
-      });
-  });
+describe("Reddit API", () => {
+  /*
+   * Test the /GET route
+   */
+  describe("/GET posts", () => {
+    it("should fetch and store Reddit posts", (done) => {
+      chai
+        .request(app)
+        .get("/api/fetch-reddit-posts")
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("success", true);
+          expect(res.body).to.have.property(
+            "message",
+            "Reddit posts fetched and stored successfully.",
+          );
+          expect(res.body).to.have.property("redditPosts");
+          expect(res.body.redditPosts).to.be.an("array");
+        });
+      done();
+    });
 
-  it("should handle errors when fetching and storing Reddit posts", function (done) {
-    chai
-      .request(server)
-      .get("/api/fetch-reddit-posts")
-      .end(function (err, res) {
-        res.should.have.status(500);
-        res.body.should.be.an("object");
-        res.body.should.have.property("success", false);
-        res.body.should.have.property(
-          "message",
-          "Error while fetching Reddit posts.",
-        );
-        done();
-      });
+    it("should handle errors when fetching Reddit posts", (done) => {
+      chai
+        .request(app)
+        .get("/api/fetch-reddit-posts")
+        .end(function (err, res) {
+          expect(res).to.have.status(500);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("success", false);
+          expect(res.body).to.have.property(
+            "message",
+            "Failed to fetch and store Reddit posts.",
+          );
+        });
+      done();
+    });
   });
 });
